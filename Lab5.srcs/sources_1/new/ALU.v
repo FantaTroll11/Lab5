bad949b74a2f8d797d43492d30835d99c44debd9
FantaTroll11 <fabian.staempfli@gmx.ch>
2026-04-20 18:53:13 +0200

    initialisation

---
+++ Lab5.srcs/sources_1/new/ALU.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2026 06:34:39 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(input [31:0] A, input [31:0] B, input [3:0] Op, output [31:0] out, output flag);
+
+
+
+endmodule
